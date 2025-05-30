85b9f07ea157b3d73bc32eb79d7e9b0e780f9a01
golin225 <issaclevi25@gmail.com>
2025-05-30 16:43:13 +0530

    modify firebase file

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require(`../../${process.env.FIREBASE_CONFIG_PATH}`);
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
