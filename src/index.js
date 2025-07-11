fdcc4949cf9fae1d1ed155f4c20a069b4ee6ad98
golin225 <issaclevi25@gmail.com>
2025-07-11 11:01:57 +0530

    cors policy

--- src/index.js
+++ src/index.js
@@ -10,11 +10,25 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://gyaan-plant-website.onrender.com',
+  'https://gyaanplant-admin-panel.onrender.com/',
+];
+
 const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  };
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 // Middlewares
 app.use(cors(corsOptions));
@@ -22,8 +36,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api', router)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
